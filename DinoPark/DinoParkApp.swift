cc03de33013835482e7205c2c2251afd6a5ee084
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-01-31 20:32:37 -0500

    feat: First commit

---
+++ DinoPark/DinoParkApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoParkApp.swift
+//  DinoPark
+//
+//  Created by Andersson on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct DinoParkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
