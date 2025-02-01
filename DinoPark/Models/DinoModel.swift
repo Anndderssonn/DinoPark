259b74b7fd6f368df3256ef45a8fe7beeca39117
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-01-31 22:06:26 -0500

    feat: Home Dino List

--- DinoPark/Models/DinoModel.swift
+++ DinoPark/Models/DinoModel.swift
@@ -5,21 +5,39 @@
 //  Created by Andersson on 31/01/25.
 //
 
-import Foundation
+import SwiftUI
 
-struct DinoModel: Decodable {
+struct DinoModel: Decodable, Identifiable {
     let id: Int
     let name: String
-    let type: String
+    let type: DinoType
     let latitude: Double
     let longitude: Double
     let movies: [String]
     let movieScenes: [MovieScene]
     let link: String
     
+    var image: String {
+        name.lowercased().replacingOccurrences(of: " ", with: "")
+    }
+    
     struct MovieScene: Decodable {
         let id: Int
         let movie: String
         let sceneDescription: String
+    }
+    
+    enum DinoType: String, Decodable {
+        case land
+        case air
+        case sea
+        
+        var backgroundColor: Color {
+            switch self {
+            case .land: return Color.brown
+            case .air: return Color.teal
+            case .sea: return Color.blue
+            }
+        }
     }
 }
