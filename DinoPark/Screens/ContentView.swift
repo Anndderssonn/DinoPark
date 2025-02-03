f452c92e2ccfe32c504d8fc8305e8fc115718d58
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-02-03 18:47:03 -0500

    feat: Create Detail Action List and Search Option

--- DinoPark/Screens/ContentView.swift
+++ DinoPark/Screens/ContentView.swift
@@ -9,29 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     let dinos = DinoDecodable()
+    @State var searchDino = ""
+    var filteredDinos: [DinoModel] {
+        if searchDino.isEmpty {
+            return dinos.dinos
+        }
+        return dinos.dinos.filter { dino in
+            dino.name.localizedCaseInsensitiveContains(searchDino)
+        }
+    }
     
     var body: some View {
-        List(dinos.dinos) { dino in
-            HStack {
-                Image(dino.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 2)
-                VStack(alignment: .leading) {
-                    Text(dino.name)
-                        .fontWeight(.bold)
-                    Text(dino.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(dino.type.backgroundColor)
-                        .clipShape(.buttonBorder)
+        NavigationStack{
+            List(filteredDinos) { dino in
+                NavigationLink {
+                    Image(dino.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        Image(dino.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 2)
+                        VStack(alignment: .leading) {
+                            Text(dino.name)
+                                .fontWeight(.bold)
+                            Text(dino.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(dino.type.backgroundColor)
+                                .clipShape(.buttonBorder)
+                        }
+                    }
                 }
-            }
-        }
-        .preferredColorScheme(.dark)
+            }.navigationTitle("Apex Predators")
+                .searchable(text: $searchDino)
+                .autocorrectionDisabled()
+                .animation(.default, value: searchDino)
+        }.preferredColorScheme(.dark)
     }
 }
 
